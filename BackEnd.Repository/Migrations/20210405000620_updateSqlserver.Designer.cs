85344a55b930280433ffa3a0e49ed36542b3f1dd
Uthant Vicentin <uthantvicentin@gmail.com>
2021-04-05 07:58:35 -0400

    Atualização Migrations SQL Server

--- BackEnd.Repository/Migrations/20210405000620_updateSqlserver.Designer.cs
+++ BackEnd.Repository/Migrations/20210405000620_updateSqlserver.Designer.cs
@@ -3,14 +3,16 @@ using BackEnd.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BackEnd.Repository.Migrations
 {
     [DbContext(typeof(BackendContext))]
-    partial class BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20210405000620_updateSqlserver")]
+    partial class updateSqlserver
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
