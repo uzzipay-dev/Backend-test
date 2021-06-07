9b9bd2fda6cdae9ed140428ee488dd064bffdb37
deodatojunior <deodatojunior999@gmail.com>
2021-06-06 20:02:03 -0400

    Correção do Docker e testes

--- TesteBackend/TesteBackend/settings.py
+++ TesteBackend/TesteBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h4xx=4t5bv-mf$^441okuh8+#s*5*h1wru74ns*n9#acks7bbj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'TesteBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
