5820bff5177d458aef38f0801c42a9762492110f
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-07 18:35:27 -0400

    feat: add user entity

---
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -0,0 +1,29 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  is_admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
