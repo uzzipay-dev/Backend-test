91d1e96705c7239e8e96dd6d4a21c1f308bd817d
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-07 18:49:24 -0400

    feat: add routes and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express';
 
-import express from 'express';
+import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
+import { router } from '@shared/routes';
 
-import createConnection from './shared/infra/typeorm';
+import '@shared/container';
 
 createConnection();
 
@@ -12,8 +15,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
-});
+app.use('/api/v1', router);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`
+    });
+  }
+);
 
 export { app };
