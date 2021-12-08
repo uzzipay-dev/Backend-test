a8d63d7af82481e2d24d255d1fb260024502ec8c
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-08 01:01:48 -0400

    feat: create table products

---
+++ src/shared/infra/typeorm/migrations/1638936857207-CreateProducts.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProducts1638936857207 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'price',
+            type: 'numeric'
+          },
+          {
+            name: 'category',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
