1e244b70982d28a2240e0ee69820665f87fa646a
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-08 01:09:42 -0400

    feat: implementation controller to create product

---
+++ src/modules/products/useCases/createProduct/CreateProductController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateProductUseCase } from './CreateProductUseCase';
+
+export class CreateProductController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, price, category } = request.body;
+
+    const createProductUseCase = container.resolve(CreateProductUseCase);
+
+    const product = await createProductUseCase.execute({
+      name,
+      price,
+      category
+    });
+
+    return response.status(201).json(product);
+  }
+}
