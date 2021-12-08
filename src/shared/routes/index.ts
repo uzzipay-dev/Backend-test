9a7ba91d3e0126720d68b2a27b322e3d16ac4573
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-08 01:11:55 -0400

    feat: add product routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import { authenticateRouter } from './authenticate.routes';
+import { productsRouter } from './products.routes';
 
 const router = Router();
 
 router.use('/session', authenticateRouter);
+
+router.use('/products', productsRouter);
 
 export { router };
