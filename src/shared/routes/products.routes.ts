0d230f7b15c6e0357c74a065a4c407a614a61a29
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-10 20:06:48 -0400

    feat: route list products

--- src/shared/routes/products.routes.ts
+++ src/shared/routes/products.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import { CreateProductController } from '@modules/products/useCases/createProduct/CreateProductController';
+import { ListProductsController } from '@modules/products/useCases/listProducts/ListProductsController';
 import { ensureAdmin } from '@shared/middlewares/ensureAdmin';
 import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticated';
 
 const createProductController = new CreateProductController();
+const listProductsController = new ListProductsController();
 
 const productsRouter = Router();
 
@@ -14,5 +16,7 @@ productsRouter.post(
   ensureAdmin,
   createProductController.handle
 );
+
+productsRouter.get('/', listProductsController.handle);
 
 export { productsRouter };
