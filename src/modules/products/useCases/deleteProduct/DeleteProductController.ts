b203a5b15f4817d962561de22522074fcd3cecbc
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-12 12:32:50 -0400

    feat: controller delete product

---
+++ src/modules/products/useCases/deleteProduct/DeleteProductController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { DeleteProductUseCase } from './DeleteProductUseCase';
+
+export class DeleteProductController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteProductUseCase = container.resolve(DeleteProductUseCase);
+
+    await deleteProductUseCase.execute(id);
+
+    return response.status(202).send();
+  }
+}
