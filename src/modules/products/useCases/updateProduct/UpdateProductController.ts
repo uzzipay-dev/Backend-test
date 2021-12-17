35a4b820b7669cc4d9cede5e509aad5202d2eb3c
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-16 20:44:47 -0400

    feat: update product controller

---
+++ src/modules/products/useCases/updateProduct/UpdateProductController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UpdateProductUseCase } from './UpdateProductUseCase';
+
+export class UpdateProductController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, category, price } = request.body;
+
+    const updateProductUseCase = container.resolve(UpdateProductUseCase);
+
+    const updatedProduct = await updateProductUseCase.execute({
+      id,
+      name,
+      price,
+      category
+    });
+
+    return response.status(201).json(updatedProduct);
+  }
+}
