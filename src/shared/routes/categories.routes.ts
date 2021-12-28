75725c868bdf9e46fed082310ac7f46bf3588f27
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-28 16:30:35 -0400

    feat: create route to delete category by id

--- src/shared/routes/categories.routes.ts
+++ src/shared/routes/categories.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import { CreateCategoryController } from '@modules/products/useCases/createCategory/CreateCategoryController';
+import { DeleteCategoryController } from '@modules/products/useCases/deleteCategory/DeleteCategoryController';
 import { ListCategoriesController } from '@modules/products/useCases/listCategories/ListCategoriesController';
 import { ensureAdmin } from '@shared/middlewares/ensureAdmin';
 import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticated';
 
 const createCategoriesController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
+const deleteCategoryController = new DeleteCategoryController();
 
 const categoriesRouter = Router();
 
@@ -18,5 +20,12 @@ categoriesRouter.post(
 );
 
 categoriesRouter.get('/', listCategoriesController.handle);
+
+categoriesRouter.delete(
+  '/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  deleteCategoryController.handle
+);
 
 export { categoriesRouter };
