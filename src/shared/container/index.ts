dd6e34285713c8c79789edbf16220e0ceb532c08
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-27 22:35:32 -0400

    feat: register singleton to categories repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,9 @@ import { container } from 'tsyringe';
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CategoriesRepository } from '@modules/products/infra/repositories/CategoriesRepository';
 import { ProductsRepository } from '@modules/products/infra/repositories/ProductsRepository';
+import { ICategoriesRepository } from '@modules/products/repositories/ICategoriesRepository';
 import { IProductsRepository } from '@modules/products/repositories/IProductsRespository';
 
 container.registerSingleton<IUsersRepository>(
@@ -13,4 +15,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IProductsRepository>(
   'ProductsRepository',
   ProductsRepository
+);
+
+container.registerSingleton<ICategoriesRepository>(
+  'CategoriesRepository',
+  CategoriesRepository
 );
