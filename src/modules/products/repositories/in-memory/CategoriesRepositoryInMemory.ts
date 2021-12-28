11b73407c0157172fc73ad91853ed6d54fe87a04
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-27 22:32:42 -0400

    feat: create repository in memory to categories

---
+++ src/modules/products/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { Category } from '@modules/products/infra/entities/Category';
+
+import { ICategoriesRepository } from '../ICategoriesRepository';
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async create(name: string): Promise<Category> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find(category => category.name === name);
+  }
+}
