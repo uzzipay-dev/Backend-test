59df442b0a547c785e3331f07bec9b1754a3d68e
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-28 14:58:09 -0400

    feat: create interface to list categories

--- src/modules/products/repositories/ICategoriesRepository.ts
+++ src/modules/products/repositories/ICategoriesRepository.ts
@@ -3,6 +3,7 @@ import { Category } from '../infra/entities/Category';
 interface ICategoriesRepository {
   create(name: string): Promise<Category>;
   findByName(name: string): Promise<Category>;
+  listAll(): Promise<Category[]>;
 }
 
 export { ICategoriesRepository };
