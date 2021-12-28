0baefbcd2d9dc08ae00dfa5b1e2b59ba792f1730
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-28 16:22:35 -0400

    feat: implentation controller to delete category

---
+++ src/modules/products/useCases/deleteCategory/DeleteCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { DeleteCategoryUseCase } from './DeleteCategoryUseCase';
+
+export class DeleteCategoryController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteCategoryUseCase = container.resolve(DeleteCategoryUseCase);
+
+    await deleteCategoryUseCase.execute({ id });
+
+    return response.status(202).send();
+  }
+}
