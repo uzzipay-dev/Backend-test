8b21c80fa446ce0391d0f59304dfa81141a906dd
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-29 15:37:34 -0400

    feat: create route patch to update category

--- src/shared/routes/categories.routes.ts
+++ src/shared/routes/categories.routes.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import { CreateCategoryController } from '@modules/products/useCases/createCategory/CreateCategoryController';
 import { DeleteCategoryController } from '@modules/products/useCases/deleteCategory/DeleteCategoryController';
 import { ListCategoriesController } from '@modules/products/useCases/listCategories/ListCategoriesController';
+import { UpdateCategoryController } from '@modules/products/useCases/updateCategory/UpdateCategoryController';
 import { ensureAdmin } from '@shared/middlewares/ensureAdmin';
 import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticated';
 
 const createCategoriesController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 const deleteCategoryController = new DeleteCategoryController();
+const updateCategoryController = new UpdateCategoryController();
 
 const categoriesRouter = Router();
 
@@ -26,6 +28,13 @@ categoriesRouter.delete(
   ensureAuthenticated,
   ensureAdmin,
   deleteCategoryController.handle
+);
+
+categoriesRouter.patch(
+  '/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  updateCategoryController.handle
 );
 
 export { categoriesRouter };
