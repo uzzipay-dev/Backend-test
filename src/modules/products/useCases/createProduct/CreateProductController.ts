4823a375b714589875bb617818b260875e59aafa
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 18:02:23 -0400

    feat: add categories_ids

--- src/modules/products/useCases/createProduct/CreateProductController.ts
+++ src/modules/products/useCases/createProduct/CreateProductController.ts
@@ -5,13 +5,14 @@ import { CreateProductUseCase } from './CreateProductUseCase';
 
 export class CreateProductController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, price } = request.body;
+    const { name, price, categories_ids } = request.body;
 
     const createProductUseCase = container.resolve(CreateProductUseCase);
 
     const product = await createProductUseCase.execute({
       name,
-      price
+      price,
+      categories_ids
     });
 
     return response.status(201).json(product);
