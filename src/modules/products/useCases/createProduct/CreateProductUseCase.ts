1fd50d3f90b8b173566ec28cb35d1a096d8320b0
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 17:51:21 -0400

    feat: add categories_ids in product creation

--- src/modules/products/useCases/createProduct/CreateProductUseCase.ts
+++ src/modules/products/useCases/createProduct/CreateProductUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { ICategoriesRepository } from '@modules/categories/repositories/ICategoriesRepository';
 import { Product } from '@modules/products/infra/entities/Product';
 import { IProductsRepository } from '@modules/products/repositories/IProductsRespository';
 import { AppError } from '@shared/errors/AppError';
@@ -7,25 +8,34 @@ import { AppError } from '@shared/errors/AppError';
 interface IRequest {
   name: string;
   price: number;
+  categories_ids: string[];
 }
 
 @injectable()
 export class CreateProductUseCase {
   constructor(
     @inject('ProductsRepository')
-    private productsRepository: IProductsRepository
+    private productsRepository: IProductsRepository,
+
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository
   ) {}
 
-  async execute({ name, price }: IRequest): Promise<Product> {
+  async execute({ name, price, categories_ids }: IRequest): Promise<Product> {
     const productAlreadyExists = await this.productsRepository.findByName(name);
 
     if (productAlreadyExists) {
       throw new AppError('product already exists');
     }
 
+    const categories = await this.categoriesRepository.findByIds(
+      categories_ids
+    );
+
     const product = await this.productsRepository.create({
       name,
-      price
+      price,
+      categories
     });
 
     return product;
