9a5027bc4647d60cb2beb3f426a1e1862675a864
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 14:39:41 -0400

    feat: create category entity

---
+++ src/modules/categories/infra/entities/Category.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('categories')
+export class Category {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
