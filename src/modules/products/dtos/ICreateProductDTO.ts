8e8b50d62bfbb33eb05e642f9aa7e5e86c3663e5
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 17:39:52 -0400

    feat: add categories

--- src/modules/products/dtos/ICreateProductDTO.ts
+++ src/modules/products/dtos/ICreateProductDTO.ts
@@ -1,6 +1,9 @@
+import { Category } from '@modules/categories/infra/entities/Category';
+
 interface ICreateProductDTO {
   name: string;
   price: number;
+  categories?: Category[];
 }
 
 export { ICreateProductDTO };
