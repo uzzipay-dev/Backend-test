16292a0d3c61bc87ea6bb6f1947cdd85dbbd639a
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 14:43:20 -0400

    feat: categories repository

---
+++ src/modules/categories/infra/repositories/CategoriesRepository.ts
@@ -0,0 +1,50 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICategoriesRepository } from '@modules/categories/repositories/ICategoriesRepository';
+
+import { Category } from '../entities/Category';
+
+export class CategoriesRepository implements ICategoriesRepository {
+  private repository: Repository<Category>;
+
+  constructor() {
+    this.repository = getRepository(Category);
+  }
+
+  async create(name: string): Promise<Category> {
+    const category = this.repository.create({ name });
+
+    await this.repository.save(category);
+
+    return category;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const category = await this.repository.findOne({ name });
+
+    return category;
+  }
+
+  async listAll(): Promise<Category[]> {
+    const categories = await this.repository.find();
+
+    return categories;
+  }
+
+  async findById(id: string): Promise<Category> {
+    const category = await this.repository.findOne({ id });
+
+    return category;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete({ id });
+  }
+
+  async update(id: string, name: string): Promise<void> {
+    await this.repository.save({
+      id,
+      name
+    });
+  }
+}
