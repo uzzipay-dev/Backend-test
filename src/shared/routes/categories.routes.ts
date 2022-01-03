e8edb4f2313ddc513f22b3786d68acb838eff0b5
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 22:15:22 -0400

    feat: create route to list products by category

--- src/shared/routes/categories.routes.ts
+++ src/shared/routes/categories.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateCategoryController } from '@modules/categories/useCases/createCategory/CreateCategoryController';
 import { DeleteCategoryController } from '@modules/categories/useCases/deleteCategory/DeleteCategoryController';
 import { ListCategoriesController } from '@modules/categories/useCases/listCategories/ListCategoriesController';
+import { ListProductsByCategoryController } from '@modules/categories/useCases/ListProductsByCategory/ListProductsByCategoryController';
 import { UpdateCategoryController } from '@modules/categories/useCases/updateCategory/UpdateCategoryController';
 import { ensureAdmin } from '@shared/middlewares/ensureAdmin';
 import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticated';
@@ -11,6 +12,7 @@ const createCategoriesController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 const deleteCategoryController = new DeleteCategoryController();
 const updateCategoryController = new UpdateCategoryController();
+const listProductsByCategoryController = new ListProductsByCategoryController();
 
 const categoriesRouter = Router();
 
@@ -36,5 +38,7 @@ categoriesRouter.patch(
   ensureAdmin,
   updateCategoryController.handle
 );
+
+categoriesRouter.post('/:id', listProductsByCategoryController.handle);
 
 export { categoriesRouter };
