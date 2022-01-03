39dd2747f36cd1001d5692c79ac3b0b6a9d32644
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-02 23:43:37 -0400

    feat: create route to list categories by product

--- src/shared/routes/products.routes.ts
+++ src/shared/routes/products.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateProductController } from '@modules/products/useCases/createProduct/CreateProductController';
 import { DeleteProductController } from '@modules/products/useCases/deleteProduct/DeleteProductController';
+import { ListCategoriesByProductController } from '@modules/products/useCases/ListCategoriesByProduct/ListCategoriesByProductController';
 import { ListProductsController } from '@modules/products/useCases/listProducts/ListProductsController';
 import { UpdateProductController } from '@modules/products/useCases/updateProduct/UpdateProductController';
 import { ensureAdmin } from '@shared/middlewares/ensureAdmin';
@@ -11,6 +12,8 @@ const createProductController = new CreateProductController();
 const listProductsController = new ListProductsController();
 const deleteProductController = new DeleteProductController();
 const updateProductController = new UpdateProductController();
+const listCategoriesByProductController =
+  new ListCategoriesByProductController();
 
 const productsRouter = Router();
 
@@ -36,5 +39,7 @@ productsRouter.patch(
   ensureAdmin,
   updateProductController.handle
 );
+
+productsRouter.post('/:id', listCategoriesByProductController.handle);
 
 export { productsRouter };
