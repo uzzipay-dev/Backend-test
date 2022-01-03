e85acb0fff5f50a11d6ad48a4b4eea9744ded167
Joao Paulo Araujo <paulointer077@gmail.com>
2022-01-03 16:15:12 -0400

    feat: add categories to update product

--- src/modules/products/dtos/IUpdateProductDTO.ts
+++ src/modules/products/dtos/IUpdateProductDTO.ts
@@ -1,7 +1,10 @@
+import { Category } from '@modules/categories/infra/entities/Category';
+
 interface IUpdateProductDTO {
   id?: string;
   name?: string;
   price?: number;
+  categories?: Category[];
 }
 
 export { IUpdateProductDTO };
